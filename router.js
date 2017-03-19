75432b9c97b8359bd9b97a0ea0c45c1421965bf2
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-03-19 13:04:12 -0700

    Added CORS

--- router.js
+++ router.js
@@ -7,7 +7,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app){
     app.get('/', requireAuth, function(req, res){
-        res.send({ hello: 'Out there!' });
+        res.send({ message: 'Here is the secret information you requested: The egg came first.' });
     });
     app.post('/signin', requireSignin, Authentication.signin);
     app.post('/signup', Authentication.signup);
