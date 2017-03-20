ecba43a8712cab8c03ecd8aed705ea40172ac49b
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-03-20 00:44:52 +0000

    Config for server

--- router.js
+++ router.js
@@ -6,9 +6,9 @@ const requireAuth = passport.authenticate('jwt', { session: false} );
 const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app){
-    app.get('/', requireAuth, function(req, res){
+    app.get('/api/', requireAuth, function(req, res){
         res.send({ message: 'Here is the secret information you requested: The egg came first.' });
     });
-    app.post('/signin', requireSignin, Authentication.signin);
-    app.post('/signup', Authentication.signup);
+    app.post('/api/signin', requireSignin, Authentication.signin);
+    app.post('/api/signup', Authentication.signup);
 };
